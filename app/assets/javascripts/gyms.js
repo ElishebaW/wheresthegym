0486066bb1509aad0049284423b734b1fab6d19c
ElishebaW <elisheba.wiggins@priviahealth.com>
2018-03-29 22:05:56 -0400

    Map with current location added.

--- app/assets/javascripts/gyms.js
+++ app/assets/javascripts/gyms.js
@@ -1,41 +1,62 @@
+/* Source: https://stackoverflow.com/questions/35340687/integrating-geo-location-api-with-google-places-api
+*/
+var map;
+var infowindow;
+var pos;
 
-      var map;
-      var infowindow;
+function initMap() {
 
-      function initMap() {
-        var pyrmont = {lat: -33.867, lng: 151.195};
+  if (navigator.geolocation) { //GEO LOCATION, FINDS USERS LOCATION
+    navigator.geolocation.getCurrentPosition(function(position) {
 
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: pyrmont,
-          zoom: 15
-        });
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pyrmont,
-          radius: 500,
-          type: ['gym']
-        }, callback);
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
       }
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: myLocation,
+        zoom: 13
+      });
+      infoWindow = new google.maps.InfoWindow({
+        map: map
+      });
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+      var myLocation = pos; //Sets variable to geo location long and lat co-ordinates.
 
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: myLocation, //Uses geolocation to find the following
+        radius: 10000,
+        types: ['gym']
+      }, callback);
+    })
+  };
 
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
+
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+initMap();
